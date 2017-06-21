fb0f4ecf5ba61f007f076d222ffdcafd3afd4043
Till Grallert <till.grallert@fu-berlin.de>
2017-06-21 11:33:52 +0200

    initial commit: map

---
+++ maps/simile.js
@@ -0,0 +1,70 @@
+{
+"items":[
+{
+"label":"Acre",
+"type":"town",
+"latlng":"32.923925,  35.070630",
+"events":"1",
+"eventType":"Food riot"},
+
+{
+"label":"Aleppo",
+"type":"town",
+"latlng":"36.20124,  37.16117",
+"events":"7",
+"eventType":"Food riot"},
+{
+"label":"Baghdad",
+"type":"town",
+"latlng":"33.34058, 44.40088",
+"events":"2",
+"eventType":"Food riot"
+},
+
+{
+"label":"Bayrūt",
+"type":"town",
+"latlng":"33.88894,  35.49442",
+"events":"4",
+"eventType":"Food riot"},
+
+{
+"label":"Dimashq",
+"type":"town",
+"latlng":"33.5102,  36.29128",
+"events":"17",
+"eventType":"Food riot"},
+
+{
+"label":"Ḥamāh",
+"type":"town",
+"latlng":"35.13179,  36.75783",
+"events":"3",
+"eventType":"Food riot"},
+
+{
+"label":"Ḥomṣ",
+"type":"town",
+"latlng":"34.72682,  36.72339",
+"events":"1",
+"eventType":"Food riot"},
+
+{
+"label":"Jaffa",
+"type":"town",
+"latlng":"32.05043, 34.75224",
+"events":"1",
+"eventType":"Food riot"}
+],
+"types": {
+		"City": {
+			"pluralLabel": "Cities"
+		}
+	},"properties": {
+		"rank": {
+			"valueType": "number"
+		},
+		"events": {
+			"valueType": "number"
+		}}
+}
